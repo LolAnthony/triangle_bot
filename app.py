0b87d4f78684b53853115409abaa734a938e473c
nsstnc <egorgolubev0484@gmail.com>
2024-10-27 16:31:43 +0300

    Added base of bot Added requirements.txt

---
+++ app.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import sys
+load_dotenv()
+
+# Получить значение токена
+TOKEN = os.getenv("TOKEN")
+
+# Диспетчер
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
